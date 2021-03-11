68d38d92a0e554d2783f1cf18bc44387d3c25503
Olga <shevtsova_olya@rambler.ru>
2021-03-11 13:23:12 +0300

    Домашнее задание 3

---
+++ HW_3_2.py
@@ -0,0 +1,4 @@
+#задание 2
+def foo(Name,Surname,YofB,city,email,phone):
+      return Name + Surname + str(YofB) + city+ email+ str(phone)
+print (foo(Name= "Имя", Surname= "Фамилия",YofB="Дата рождения",city= "город рождения",email="почта",phone= "телефон"))
