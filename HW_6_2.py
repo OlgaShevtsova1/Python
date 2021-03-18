1cba09a9289ad2988cf04489d9b7464907ff8149
Olga <shevtsova_olya@rambler.ru>
2021-03-18 22:56:59 +0300

    Домашнее задание 6

---
+++ HW_6_2.py
@@ -0,0 +1,14 @@
+# урок 6 задание 2
+class Road:
+    _length="length"
+    _width="width"
+    def __init__(self,length,width):
+        self._width=width
+        self._length=length
+r=Road(length=20,width=5000)
+m=25
+depth=5
+print(20*5000*25*0.05)
+
+
+
